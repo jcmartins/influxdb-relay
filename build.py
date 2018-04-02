614f764668d79ad8a9aa5093b77a4b6435f8d817
Joao Martins <jcmartins@gmail.com>
2018-04-02 12:56:54 -0300

    change gdm to godeps

--- build.py
+++ build.py
@@ -125,13 +125,13 @@ def go_get(branch, update=False, no_uncommitted=False):
     if local_changes() and no_uncommitted:
         logging.error("There are uncommitted changes in the current directory.")
         return False
-    if not check_path_for("gdm"):
-        logging.info("Downloading `gdm`...")
-        get_command = "go get github.com/sparrc/gdm"
+    if not check_path_for("godep"):
+        logging.info("Downloading `godep`...")
+        get_command = "go get github.com/tools/godep"
         run(get_command)
-    logging.info("Retrieving dependencies with `gdm`...")
+    logging.info("Retrieving dependencies with `godep`...")
     sys.stdout.flush()
-    run("{}/bin/gdm restore -v".format(os.environ.get("GOPATH")))
+    run("{}/bin/godep restore -v".format(os.environ.get("GOPATH")))
     return True
 
 def run(command, allow_failure=False, shell=False):
